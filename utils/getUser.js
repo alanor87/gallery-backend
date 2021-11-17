a6fa004bbffe7a454e9a600d1fce96dbb358102f
itmaxxx <dmitrievmaks77@gmail.com>
2021-11-17 14:47:43 +0200

    Test commit

--- utils/getUser.js
+++ utils/getUser.js
@@ -1,9 +1,9 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 const getUser = async (filter) => {
-    const user = await User.findOne(filter);
-    return user;
-
-}
+  console.log("User");
+  const user = await User.findOne(filter);
+  return user;
+};
 
 module.exports = getUser;
