3c2f9970cbb8a7751d7d7c489df6ae54d711464f
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-11-24 22:28:26 +0200

    Bugfix with password compare function

--- controllers/users/getUserByToken.js
+++ controllers/users/getUserByToken.js
@@ -2,7 +2,8 @@ const { getUser } = require('../../utils');
 
 const getUserByToken = async (req, res, next) => {
   console.log(req.userId);
-  const userDataToSend = await getUser({ _id: req.userId });
+  const user = await getUser({ _id: req.userId });
+  const userDataToSend = user.toObject();
   delete userDataToSend.userPassword;
   res.status(200)
     .json({
--- utils/getUser.js
+++ utils/getUser.js
@@ -2,7 +2,7 @@ const { User } = require("../models");
 
 const getUser = async (filter) => {
     const user = await User.findOne(filter).populate('userOwnedImages');
-    return user.toObject();
+    return user;
 }
 
 module.exports = getUser;
