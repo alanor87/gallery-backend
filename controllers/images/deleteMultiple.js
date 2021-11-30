c147bd621bc4db4e5e9b6f37dc24dbd8babe90fe
alanor87 <jonny.freeman321@gmail.com>
2021-11-30 09:10:29 +0200

    Multiple images deletion, trouble with editing userOwnedImages list

--- controllers/images/deleteMultiple.js
+++ controllers/images/deleteMultiple.js
@@ -1,20 +1,57 @@
 const { Image, User } = require("../../models");
+const { cloudinary } = require("../../utils");
+const { getUser } = require("../../utils");
 
 const deleteMultiple = async (req, res, next) => {
-    try {
-        const deleteRequests = req.body.imagesIdToDelete.map(_id => Image.findByIdAndDelete(_id));
-        const result = await Promise.all(deleteRequests);
-        console.log('result : ', result);
-        res.status(200).json({
-            status: 'Success',
-            code: 200,
-            message: 'Images deleted.',
-        });
-    }
-    catch (error) {
-        error.message = 'Error while deleting multiple images.';
-        next(error);
-    }
+  try {
+    const { imagesToDelete } = req.body;
+    const deleteIdList = imagesToDelete.map((image) => image.selectedId);
+    const deleteHostingIdList = imagesToDelete.map(
+      (image) => image.imageHostingId
+    );
+
+    /*
+     * Deleting images from DB.
+     */
+    const deleteDBRequests = deleteIdList.map((_id) =>
+      Image.findByIdAndDelete(_id)
+    );
+    const resultDB = await Promise.all(deleteDBRequests);
+    console.log("Delete from DB : ", resultDB);
+
+    /*
+     * Deleting images from hosting.
+     */
+    const deleteHostingRequests = deleteHostingIdList.map((hostingId) =>
+      cloudinary.api.delete_resources(hostingId)
+    );
+    const resultHosting = await Promise.all(deleteHostingRequests);
+    console.log("Delete from hosting : ", resultHosting);
+
+    /*
+     * Deleting images IDs from userOwnedImages in User object.
+     */
+    const currentUser = await User.findById(req.userId);
+    const { userOwnedImages } = currentUser;
+    console.log("currentUserOwnedImages : ", userOwnedImages);
+    const newUserOwnedImagesList = userOwnedImages.filter(
+      (imageId) => !deleteIdList.includes(imageId)
+    );
+    console.log("newUserOwnedImagesList : ", newUserOwnedImagesList);
+    currentUser.userOwnedImages = newUserOwnedImagesList;
+    const updatedUser = await currentUser.save();
+    console.log("updatedUser : ", updatedUser);
+
+    res.status(200).json({
+      status: "Success",
+      code: 200,
+      message: "Images deleted.",
+      newImagesList: updatedUser.userOwnedImages,
+    });
+  } catch (error) {
+    error.message = "Error while deleting multiple images.";
+    next(error);
+  }
 };
 
 module.exports = deleteMultiple;
